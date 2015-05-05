bcd838e9d7a4dc95467d5d121f769f3777a743bc
cosmin <cgodeanu@mail.com>
2015-05-05 16:43:04 +0200

    Different package name.

---
+++ src/threads/safety/NameList.java
@@ -0,0 +1,33 @@
+package threads.safety;
+
+import java.util.*;
+
+public class NameList {
+	private List<String> names = Collections.synchronizedList(new LinkedList<String>());
+
+	public void add(String name) {
+		names.add(name);
+	}
+
+	public String removeFirst() {
+		if (names.size() > 0)
+			return (String) names.remove(0);
+		else
+			return null;
+	}
+
+	public static void main(String[] args) {
+		final NameList nl = new NameList();
+		nl.add("Ozymandias");
+		class NameDropper extends Thread {
+			public void run() {
+				String name = nl.removeFirst();
+				System.out.println(name);
+			}
+		}
+		Thread t1 = new NameDropper();
+		Thread t2 = new NameDropper();
+		t1.start();
+		t2.start();
+	}
+}
