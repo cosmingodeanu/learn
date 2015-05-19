0965fb5176adf13b660064ce08c16376df7dae97
cosmin <cgodeanu@mail.com>
2015-05-19 17:55:27 +0200

    Characters are weird

---
+++ src/operators/Chars.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class Chars {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char c ='q';
+		System.out.println("c>3.5 is "+(c>3.5f));
+		System.out.println("3.5>3l is "+(3.5>3l));
+	}
+
+}
